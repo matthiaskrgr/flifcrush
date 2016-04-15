a34326b011d555ab0dc9d07c314071d564f62206
Matthias Krüger <matthias.krueger@famsik.de>
2016-04-15 15:29:41 +0200

    add inline check that ensures we have python 3

--- main.py
+++ main.py
@@ -17,6 +17,10 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
 
+import sys
+if (sys.version_info.major != 3): # no python 3
+	print("Python 3 or higher is required.")
+	sys.exit(1)
 
 import subprocess
 import sys
